01a51a563f820efea485eca3f179427c3f9de2c7
Nemanja Delic <NemanjaD@ccbill.com>
2016-05-22 11:52:33 +0200

    adding kernel params

---
+++ site/profile/manifests/common/kernel.pp
@@ -0,0 +1,17 @@
+#
+class profile::common::kernel {
+  $kernel_params=lookup( 'common::kernel_params' )
+
+  $kernel_params | $name, $kernel_parameter | {
+    kernel_parameter { $name:
+      ensure => $kernel_parameter[ 'ensure' ],
+      value  => $kernel_parameter[ 'value' ],
+    }
+  }
+
+  reboot { 'after_run':
+    apply => 'finished'
+  }
+
+  Kernel_parameter <| |> ~> Reboot <| |>
+}
